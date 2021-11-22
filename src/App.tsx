aadec758d430dde85cfe61fe16bce14e25449642
Blessedadudu <adududuke@gmail.com>
2021-11-22 10:57:47 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { lazy } from "react"
 import './App.css';
 import { Switch, Route } from "react-router-dom";
 import Home from './components/Home/Home'
-import Films from './components/roots/Films/Films'
+// import Films from './components/roots/Films/Films'
 import People from './components/roots/People/People'
 import Planets from './components/roots/Planets/Planets'
 import Starship from './components/roots/Starship/Starship';
+
+const Films = lazy(() => import('./components/roots/Films/Films'))
+// const Films = lazy(() => import('./components/roots/Films/Films'))
+// const Films = lazy(() => import('./components/roots/Films/Films'))
+// const Films = lazy(() => import('./components/roots/Films/Films'))
 
 
 function App() {
