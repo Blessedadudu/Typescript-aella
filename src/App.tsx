91ad91169662dcd70f017c3d4f1bcc8b10d6f122
Blessedadudu <adududuke@gmail.com>
2021-11-22 11:04:46 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { lazy } from "react"
+import { lazy, Suspense } from "react"
 import './App.css';
 import { Switch, Route } from "react-router-dom";
 import Home from './components/Home/Home'
+import Spinner from "./Spinner/Spinner";
 // import Films from './components/roots/Films/Films'
 import People from './components/roots/People/People'
 import Planets from './components/roots/Planets/Planets'
@@ -18,7 +19,9 @@ function App() {
     <div className="App">
       <Switch> 
             <Route exact path='/' component={Home} />
-            <Route path='/films' component={Films} />
+            <Suspense fallback={<Spinner/>}>
+              <Route path='/films' component={Films} />
+            </Suspense>
             <Route path='/people' component={People} />
             <Route path='/planets' component={Planets} />
             <Route path='/spaceships' component={Starship} />
